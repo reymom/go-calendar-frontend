dfb145b4ddf9f7c8c5de969c10dde080f371d019
reymom <ramon_gs95@hotmail.com>
2023-08-05 21:31:24 +0200

    template functions

--- internal/routes/templates.go
+++ internal/routes/templates.go
@@ -1,7 +1,33 @@
 package routes
 
-import "html/template"
+import (
+	"html/template"
+
+	"github.com/rs/zerolog/log"
+)
 
 func newFuncMap() template.FuncMap {
-	return template.FuncMap{}
+	return template.FuncMap{
+		"dict":    passDictInTemplate,
+		"FmtWeek": fmtWeek,
+	}
+}
+
+func passDictInTemplate(values ...interface{}) map[string]interface{} {
+	if len(values)%2 != 0 {
+		log.Err(nil).Msg("invalid dict call")
+	}
+	dict := make(map[string]interface{}, len(values)/2)
+	for i := 0; i < len(values); i += 2 {
+		key, ok := values[i].(string)
+		if !ok {
+			log.Err(nil).Msg("dict keys must be strings")
+		}
+		dict[key] = values[i+1]
+	}
+	return dict
+}
+
+func fmtWeek(isoWeek int) string {
+	return ""
 }
